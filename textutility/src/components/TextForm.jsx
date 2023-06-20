ec1a3ad9430a539dac72542bb12f8cb5f2dde025
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-06-20 11:44:22 +0530

    Updated the count logic to count words in next line and inverted the about arrow color

--- textutility/src/components/TextForm.jsx
+++ textutility/src/components/TextForm.jsx
@@ -192,13 +192,13 @@ export default function TextForm(props) {
         <h3>Text Summary</h3>
         <p>
           {" "}
-          {text.split(" ").filter((element) => {
+          {text.split(/\s+/).filter((element) => {
             return element.length!==0
           }).length} words and {text.length} characters{" "}
         </p>
         <p>
           {" "}
-          {0.008 * text.split(" ").filter((element) => {
+          {0.008 * text.split(/\s+/).filter((element) => {
             return element.length!==0
           }).length} minutes is required to read the given
           text{" "}
